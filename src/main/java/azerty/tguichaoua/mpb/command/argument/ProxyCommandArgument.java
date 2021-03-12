1bf09eee88d2ee45bb38a7e7054e82be0d12a13c
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2021-03-12 17:44:18 +0100

    🏷️ add ProxyCommandArgument

---
+++ src/main/java/azerty/tguichaoua/mpb/command/argument/ProxyCommandArgument.java
@@ -0,0 +1,21 @@
+package azerty.tguichaoua.mpb.command.argument;
+
+import azerty.tguichaoua.mpb.command.CommandException;
+import azerty.tguichaoua.mpb.command.CommandExecution;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public abstract class ProxyCommandArgument<T, R> implements CommandArgument<R> {
+	protected final CommandArgument<T> source;
+
+	@Override public @NotNull Collection<String> complete(@NotNull final CommandExecution execution) throws CommandException {
+		return source.complete(execution);
+	}
+
+	@Override public int multiplicity() {
+		return source.multiplicity();
+	}
+}
--- src/main/java/azerty/tguichaoua/mpb/command/argument/TransformCommandArgument.java
+++ src/main/java/azerty/tguichaoua/mpb/command/argument/TransformCommandArgument.java
@@ -2,26 +2,21 @@ package azerty.tguichaoua.mpb.command.argument;
 
 import azerty.tguichaoua.mpb.command.CommandException;
 import azerty.tguichaoua.mpb.command.CommandExecution;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
+public final class TransformCommandArgument<T, R> extends ProxyCommandArgument<T, R> {
+	private final @NotNull Function<T, R> function;
 
-@RequiredArgsConstructor
-public final class TransformCommandArgument<T, R> implements CommandArgument<R> {
-	private final CommandArgument<T> source;
-	private final Function<T, R> function;
+	public TransformCommandArgument(
+			@NotNull final CommandArgument<T> source,
+			@NotNull final Function<T, R> function
+	) {
+		super(source);
+		this.function = function;
+	}
 
 	@Override public R parse(@NotNull final CommandExecution execution) throws CommandException {
 		return function.apply(execution, source.parse(execution));
-	}
-
-	@Override public @NotNull Collection<String> complete(@NotNull final CommandExecution execution) throws CommandException {
-		return source.complete(execution);
-	}
-
-	@Override public int multiplicity() {
-		return source.multiplicity();
 	}
 
 	@FunctionalInterface
